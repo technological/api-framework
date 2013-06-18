701601150f9852d73e3fbc6e0de317b86dd79fd7
Martin Bean <martin@martinbean.co.uk>
2013-06-18 15:03:50 +0100

    Created crude test controller

---
+++ src/MCB/Api/Controller/TestController.php
@@ -0,0 +1,10 @@
+<?php
+namespace MCB\Api\Controller;
+
+class TestController
+{
+    public function get()
+    {
+        echo 'Hello, world!';
+    }
+}
--- src/MCB/Api/Framework/Application.php
+++ src/MCB/Api/Framework/Application.php
@@ -3,8 +3,32 @@ namespace MCB\Api\Framework;
 
 class Application
 {
-    public function __construct()
+    public function run()
     {
-        echo 'Hello, world.';
+        $parameters = $this->getRequestParameters($_SERVER['REQUEST_METHOD']);
+        
+        $controller_instance = new \MCB\Api\Controller\TestController();
+        $method = 'get';
+        
+        call_user_func_array(array($controller_instance, $method), $parameters);
+    }
+    
+    private function getRequestParameters($method)
+    {
+        $parameters = array();
+        
+        switch (strtoupper($method)) {
+            case 'GET':
+                $parameters = $_GET;
+            break;
+            case 'POST':
+                $parameters = $_POST;
+            break;
+            case 'PUT':
+                parse_str(file_get_contents('php://input', $parameters));
+            break;
+        }
+        
+        return $parameters;
     }
 }
